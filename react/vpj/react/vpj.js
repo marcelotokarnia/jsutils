c43ef279e8f996a3fe7f7115fef924aa0ec9a0cc
Bruno Dias <bruno.dias@vtex.com.br>
2018-05-25 20:04:04 -0300

    Fix pages issue

--- react/vpj/react/vpj.js
+++ react/vpj/react/vpj.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import query from './queries/products.gql'
 
 class VPJ extends Component {
+
   static schema = {
     title: 'VPJ',
     description: 'VTEX PARTNER JOURNEY',
@@ -16,21 +17,21 @@ class VPJ extends Component {
   }
 
   render() {
-    const { productsQuery : { products }, showImages } = this.props
-    return <div><ul>{products.map(({productName, productId, items}) => <li key={productId}>
-        <h3>{productName}</h3>
-        <ul>{items.map(({itemId, images, name}) => (
-          <li key={itemId}>
-            <h5>{name}</h5>
-            { showImages && (<ul>{images.map(({imageId, imageUrl}) => (
-              <li key={imageId}>
-                <img src={imageUrl}/>
-              </li>
-            ))}</ul>) }
-          </li>
-        ))}</ul>
-      </li>)}</ul></div>
+    const { productsQuery: { products }, showImages } = this.props
+    return <div><ul>{products.map(({ productName, productId, items }) => <li key={productId}>
+      <h3>{productName}</h3>
+      <ul>{items.map(({ itemId, images, name }) => (
+        <li key={itemId}>
+          <h5>{name}</h5>
+          {showImages && (<ul>{images.map(({ imageId, imageUrl }) => (
+            <li key={imageId}>
+              <img src={imageUrl} />
+            </li>
+          ))}</ul>)}
+        </li>
+      ))}</ul>
+    </li>)}</ul></div>
   }
 }
 
-export default graphql(query, {name: 'productsQuery'})(VPJ)
+export default graphql(query, { name: 'productsQuery' })(VPJ)
